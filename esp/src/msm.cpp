98ace3219cd15c4d23a58b5ebfa92c5446dc174e
aXe <75217743+aXe-88@users.noreply.github.com>
2022-08-17 23:25:58 +0200

    Update msm.cpp

--- esp/src/msm.cpp
+++ esp/src/msm.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
 #include <StateMachine.h>
 
+// StateMachine Lib: https://github.com/jrullan/StateMachine
+// LinkedList Lib: https://github.com/ivanseidel/LinkedList
+
 StateMachine machine = StateMachine();
